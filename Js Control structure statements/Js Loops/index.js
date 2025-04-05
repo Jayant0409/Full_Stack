e1e00acf6e346f33cd2f77886f77be447c6c8dcb
Jayant <004jayant@gmail.com>
2025-04-05 23:46:54 +0530

    Loops and if_else statments

---
+++ Js Control structure statements/Js Loops/index.js
@@ -0,0 +1,39 @@
+console.log("hello i  am a loop tutorial")
+
+let a=1
+// for (let i = 0;i<100; i++) {
+//     const element = a+i;                         // for loop
+//     console.log(element)
+
+// }
+
+// let obj={ 
+//     nam:"jayant",
+//     role: "Programmer",
+//     company:"CWD" 
+// }
+// for(let key in obj){
+//      const element = obj[key]                  // forin lop
+//      console.log(key,element)
+// }
+
+// for (const c of "Harry") {
+//     console.log(c)                              // forof loop
+
+// }
+
+// let i=0;
+// while(i<50){                              // while loop
+//     console.log(i)
+//     i++
+// }
+
+
+let i=10;
+do
+{                                          // do while loop
+    console.log(i)
+    i++;
+
+}
+while(i<5);
---
+++ Js Control structure statements/js if else, else if ladder/index.js
@@ -0,0 +1,14 @@
+console.log(" Hello I am conditional tutorial")
+
+let age = 45;
+let b = 2
+console.log(b**b)
+
+if(age==0){
+    console.log("you are kidding")
+}
+else if(age==45){
+    console.log("you are again kidding")
+}
+else
+console.log("you are not kidding")

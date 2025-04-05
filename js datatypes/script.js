b2e78307aa798bb51f0da41b6d358ea56fcaa7cd
Jayant <004jayant@gmail.com>
2025-04-05 23:39:17 +0530

    Javascript starting

---
+++ js datatypes/script.js
@@ -0,0 +1,37 @@
+console.log("hey js")
+
+let a1=58          // let has global scope         
+var a=5            // var has block scope
+var b=9            // const value can't be changed
+const d =45
+//d=99
+let c="jay"
+console.log(d)
+
+console.log(a - b + "String")
+console.log(typeof a,typeof b,typeof c)
+
+{
+    var a= 99
+    console.log(a)
+    let a1=7
+    console.log(a1)
+}
+
+console.log(a1)
+
+//objects in js
+let o = {                      // we have created object o
+    nam: "jayant",
+"job code":5600,
+hsndsome:true 
+}
+
+console.log(o);
+o.salary="100crore"           // adding more value in object
+console.log(o);
+o.salary="500cr"
+console.log(o);
+
+
+

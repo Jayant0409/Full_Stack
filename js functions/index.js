3002e05dda431e5c48bfd2d0d48aaa81307c4e2d
Jayant <004jayant@gmail.com>
2025-04-05 23:58:08 +0530

    Functions

---
+++ js functions/index.js
@@ -0,0 +1,21 @@
+function nice(nam){
+
+console.log("hey "+ nam +" you are good")
+}
+
+function sum(a,b,c=4){
+    console.log("I am a sum function"+a+b+c)
+
+}
+
+nice("jayant")
+sum(4,8,9)
+
+const func = (x)=> {
+    
+    console.log("I am an arraow function "+x)
+    
+}
+
+func(94)
+func(20)
---
+++ js functions/tempCodeRunnerFile.js
@@ -0,0 +1,8 @@
+const func = (x)=> {
+    
+    console.log(x)
+    
+}
+
+func(94)
+func(20)

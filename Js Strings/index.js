c6376679b7f83b55e61967e1114e459c3e3babad
Jayant <004jayant@gmail.com>
2025-04-06 23:53:50 +0530

    Strings in Js

---
+++ Js Strings/index.js
@@ -0,0 +1,26 @@
+console.log("This is string tutorial")
+let a="Harry";
+console.log(a[2])
+console.log(a[0])
+
+console.log(a.length)
+
+let nam ="jayant"
+let friend="Rohan"
+
+console.log("His name is " + nam +"and his friend name is "+friend)
+console.log(`His name is ${nam} and his friend name is ${friend}`)
+
+let b="Shivam"
+console.log(b.toUpperCase())
+console.log(b.toLocaleLowerCase())
+
+console.log(b.slice(1,5))           // like substring  ,in this 1 is included and 5 is not included 
+
+console.log(b.replace("Sh","77"))
+console.log(b.concat(a))
+console.log(b.concat(a,"Rahul","Jayant"))
+
+let n="Harry   "
+console.log(n.indexOf('H'))
+
